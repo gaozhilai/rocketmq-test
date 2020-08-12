3db8ffddb9074cd125b363dab2d899d3a39d66f4
gaozhilai <gaozhilai@thunisoft.com>
2020-08-12 18:13:25 +0800

    更改starter方式消费消息tag过滤方式

--- src/main/java/com/gzl/rocketmq/consumer/UserConsumer.java
+++ src/main/java/com/gzl/rocketmq/consumer/UserConsumer.java
@@ -1,6 +1,7 @@
 package com.gzl.rocketmq.consumer;
 
 import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
+import org.apache.rocketmq.spring.annotation.SelectorType;
 import org.apache.rocketmq.spring.core.RocketMQListener;
 import org.springframework.stereotype.Service;
 
@@ -16,7 +17,8 @@ import lombok.extern.slf4j.Slf4j;
  * @date 2020/8/12 17:53
  */
 @Service
-@RocketMQMessageListener(topic = ConfigConst.TOPIC_NAME, consumerGroup = "user_consumer", selectorExpression = "user_producer")
+@RocketMQMessageListener(topic = ConfigConst.TOPIC_NAME, consumerGroup = "user_consumer",
+        selectorType = SelectorType.TAG, selectorExpression = "user_producer")
 @Slf4j
 public class UserConsumer implements RocketMQListener<User> {
 
